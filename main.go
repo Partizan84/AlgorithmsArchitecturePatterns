aca082ee3ef63305a1dcdef6e1d32c4e94cfbb4f
Partizan84 <knazarov84@gmail.com>
2021-04-17 02:15:34 +0500

    создание и обновление ДЗ. Без обращения к БД.

---
+++ main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"math/rand"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
+
+type Item struct {
+	ID          (string) `json:"id"`
+	Name        (string) `json:"name"`
+	Description (string) `json:"description"`
+}
+
+var items []Item
+
+func getItems(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(items)
+}
+
+func getItem(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for _, item := range items {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(&Item{})
+}
+
+func postItem(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var item Item
+	_ = json.NewDecoder(r.Body).Decode(&item)
+	item.ID = strconv.Itoa(rand.Intn(1000000))
+	items = append(items, item)
+	json.NewEncoder(w).Encode(item)
+}
+
+func deleteItem(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range items {
+		if item.ID == params["id"] {
+			items = append(items[:index], items[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(items)
+}
+
+func main() {
+	r := mux.NewRouter()
+	items = append(items, Item{ID: "1", Name: "Георгин", Description: "Цветок"})
+	items = append(items, Item{ID: "2", Name: "Пион", Description: "Цветок"})
+	r.HandleFunc("/items", getItem).Methods("GET")
+	r.HandleFunc("/items/{id}", getItem).Methods("GET")
+	r.HandleFunc("/items", postItem).Methods("POST")
+	r.HandleFunc("/items/{id}", deleteItem).Methods("DELETE")
+	log.Fatal(http.ListenAndServe(":8000", r))
+}
