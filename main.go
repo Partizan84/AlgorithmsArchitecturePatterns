84ae5131f91b2b5aca2dfafc6b3eeb0ea2d1f951
Partizan84 <knazarov84@gmail.com>
2021-04-28 01:22:35 +0500

    Создание папок.

--- main.go
+++ main.go
@@ -15,56 +15,11 @@ import (
 
 	"fmt"
 	"github.com/crafter76/newmod"
-	"example.com/m"
 )
 
 //var items []Item
 
-type myHandler struct {
 
-	func getItems(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(items)
-	}
-
-	func getItem(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
-		params := mux.Vars(r)
-		for _, item := range items {
-			if item.ID == params["id"] {
-				json.NewEncoder(w).Encode(item)
-				return
-			}
-		}
-		json.NewEncoder(w).Encode(&Item{})
-	}
-
-	func postItem(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
-		var item Item
-		_ = json.NewDecoder(r.Body).Decode(&item)
-		item.ID = strconv.Itoa(rand.Intn(1000000))
-		items = append(items, item)
-		json.NewEncoder(w).Encode(item)
-	}
-
-	func deleteItem(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
-		params := mux.Vars(r)
-		for index, item := range items {
-			if item.ID == params["id"] {
-				items = append(items[:index], items[index+1:]...)
-				break
-			}
-		}
-		json.NewEncoder(w).Encode(items)
-	}
-
-}
-
-func (h myHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte(`Структура объединяющая handler-функции`))
-}
 
 func main() {
 	fmt.Println(newmod.Hi("Василий"))
@@ -72,10 +27,6 @@ func main() {
 	items = append(items, Item{ID: "1", Name: "Георгин", Description: "Цветок"})
 	items = append(items, Item{ID: "2", Name: "Пион", Description: "Цветок"})
 	http.Handle("/", myHandler{})
-	//r.HandleFunc("/items", getItem).Methods("GET")
-	//r.HandleFunc("/items/{id}", getItem).Methods("GET")
-	//r.HandleFunc("/items", postItem).Methods("POST")
-	//r.HandleFunc("/items/{id}", deleteItem).Methods("DELETE")
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
 
